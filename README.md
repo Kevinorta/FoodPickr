0a5c4d3109f396d9125b93c72c4300b8a7f62a4a
Kevinorta <kevino6532@hstat.org>
2019-11-21 02:01:54 -0500

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# FoodPickr
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,87 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInput: "",
+      list: []
+    };
+  }
+
+  changeUserInput(input) {
+    this.setState({
+      userInput: input
+    });
+  }
+  addFood(input) {
+    let foodArray = this.state.list;
+
+    foodArray.push(input);
+    this.setState({
+      list: foodArray,
+      userInput: ""
+    });
+  }
+  remove(val) {
+    let foodArray = this.state.list;
+    foodArray = foodArray.filter(e => e !== val);
+    this.setState({
+      list: foodArray,
+      userInput: ""
+    });
+  }
+  pickFood(list) {
+    let pickedFood = list[Math.floor(Math.random() * list.length)];
+    alert(pickedFood);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <input
+          type="text"
+          placeholder="Type Food Here"
+          value={this.state.userInput}
+          onChange={e => {
+            this.changeUserInput(e.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            this.addFood(this.state.userInput);
+          }}
+        >
+          Add Food
+        </button>
+        <button
+          onClick={() => {
+            this.pickFood(this.state.list);
+          }}
+        >
+          Pick Food
+        </button>
+        <ul>
+          {this.state.list.map(val => (
+            <ul>
+              <li>{val}</li>
+              <button
+                onClick={() => {
+                  this.remove(val);
+                }}
+              >
+                X
+              </button>
+            </ul>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
